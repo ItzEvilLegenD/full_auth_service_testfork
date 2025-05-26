e7805a4b95c304211500054c793240849a60883d
ItzEvilLegenD <165904505+ItzEvilLegenD@users.noreply.github.com>
2025-05-26 15:53:26 +0300

    Delete auth_folder directory

--- auth_folder/config.py
+++
@@ -1,9 +0,0 @@
-from authx import AuthXConfig
-
-
-
-config = AuthXConfig()
-config.JWT_SECRET_KEY = 'secret_key'
-config.JWT_ACCESS_COOKIE_NAME = 'token'
-config.JWT_TOKEN_LOCATION = ['cookies']
-
--- auth_folder/models/user.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class User(BaseModel):
-    user_id: int
-    name: str
-    age: int
-    email: str
-    role: str
